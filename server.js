46cd8e9718243df0842acd2f92a74e74b8b9c4b1
Andrew Johnston <andrew.johnston@shopify.com>
2018-12-10 17:13:33 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const Koa = require('koa');
+const next = require('next');
+const Router = require('koa-router');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const dotenv = require('dotenv');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+require('isomorphic-fetch');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+dotenv.config();
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+  server.use(session(server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  router.get('*', async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products'],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+
+        console.log('We did it!', shop, accessToken);
+
+        ctx.redirect('/');
+      },
+    }),
+  );
+
+  server.use(router.routes());
+  server.use(verifyRequest());
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
